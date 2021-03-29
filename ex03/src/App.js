697ae589e858d05a7fb5bfe2e2870547b25e4154
aanc1122 <amelanuhanovic997@gmail.com>
2021-03-29 21:31:31 +0200

    Class and Simple Components

---
+++ ex03/src/App.js
@@ -0,0 +1,13 @@
+import Plant from './Plant';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Plant />
+    </div>
+  );
+}
+
+export default App;
---
+++ ex10/src/Box.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+function Box(props) {
+    
+    return <h2>This is a {props.size}px Box!</h2>;
+    
+}
+
+
+export default Box;
